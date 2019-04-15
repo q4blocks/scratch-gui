26131fa6705cfab76d300161bb55caf8f9a1f4d6
Peeratham Techapalokul <tpeera4@vt.edu>
2019-04-15 10:23:05 -0400

    wip

--- src/components/tutorial/tutorial.jsx
+++ src/components/tutorial/tutorial.jsx
@@ -4,6 +4,35 @@ import '!style-loader!css-loader!../../css/steps-iconfont.css';
 
 import Steps, { Step } from 'rc-steps';
 
+import { Popper } from 'react-popper';
+import Floater from 'react-floater';
+class VirtualReference {
+    getBoundingClientRect() {
+        return {
+            top: 10,
+            left: 10,
+            bottom: 20,
+            right: 100,
+            width: 90,
+            height: 10,
+        };
+    }
+
+    get clientWidth() {
+        return this.getBoundingClientRect().width;
+    }
+
+    get clientHeight() {
+        return this.getBoundingClientRect().height;
+    }
+}
+
+// This is going to create a virtual reference element
+// positioned 10px from top and left of the document
+// 90px wide and 10px high
+const virtualReferenceElement = new VirtualReference();
+
+
 const steps = [
     {
         title: 'Create a custom block',
@@ -17,12 +46,28 @@ const steps = [
     }
 ]
 
+const target = document.querySelector(".scratchCategoryMenu > div:nth-child(9)");
+
 const Tutorial = props => (
-    <Steps current={0} direction={'horizontal'}>
-        {steps.map(step => (
-            <Steps.Step title={step.title} description={step.descriptions} />
-        ))}
-    </Steps>
+    <div className={'firstStep'}>
+        <Steps current={0} direction={'horizontal'}>
+            {steps.map((step, key) => (
+                <Steps.Step key={key} title={step.title} description={step.descriptions} />
+            ))}
+        </Steps>
+
+        <Popper referenceElement={props.target ? props.target : virtualReferenceElement} placement="right">
+            {({ ref, style, placement, arrowProps }) => (
+                
+                    <div ref={ref} style={{...style, zIndex:100}} data-placement={placement}>
+                        <span style={{ color: '#f04', fontSize: 34, zIndex: 100 }}>◉</span>
+                        <div ref={arrowProps.ref} style={{...arrowProps.style}} />
+                    </div>
+            )}
+        </Popper>
+
+
+    </div>
 );
 
 export default Tutorial;
--- src/containers/tutorial.jsx
+++ src/containers/tutorial.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import TutorialComponent from '../components/tutorial/tutorial.jsx';
 
 class Tutorial extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state = { target: null };
     }
+
+    componentDidMount() {
+        setTimeout(() => {
+            const myBlockTarget = document.querySelector(".scratchCategoryMenu > div:nth-child(9)");
+            const makeBlockTarget = document.querySelectorAll(".blocklyFlyoutButton")[2];
+            this.setState({
+                target: myBlockTarget
+            });
+            myBlockTarget.addEventListener("click", ()=>{console.log("My Block!")});
+            makeBlockTarget.addEventListener("click", ()=>{console.log("Make a Block!")});
+            console.log('set target');
+        }, 5000);
+    }
+
     render() {
-        return (<TutorialComponent />);
+        return (<TutorialComponent target={this.state.target} />);
     }
 }
 
