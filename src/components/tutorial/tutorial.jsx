3e227e7bd2779bb6f23323dc0d00069e0e1d48bb
Peeratham Techapalokul <tpeera4@vt.edu>
2019-04-18 15:59:03 -0400

    improve custom tooltip/modal content

--- src/components/tutorial/tutorial.jsx
+++ src/components/tutorial/tutorial.jsx
@@ -9,6 +9,8 @@ import classnames from 'classnames';
 
 import { Popper } from 'react-popper';
 import Floater from 'react-floater';
+
+import renderHTML from 'react-render-html';
 class VirtualReference {
     getBoundingClientRect() {
         return {
@@ -35,8 +37,6 @@ class VirtualReference {
 // 90px wide and 10px high
 const virtualReferenceElement = new VirtualReference();
 
-
-
 const beaconStyle = {
     animation: 'BeaconFlick 1s infinite',
     color: '#7FB800',
@@ -44,59 +44,96 @@ const beaconStyle = {
     cursor: 'default'
 }
 
+const modalMaxSize = {
+    'default': 500,
+    'large': 1000
+}
+
+const modalContainerStyle = {
+    width: '40vw',
+    // height: '30vh',
+    padding: '2rem',
+    background: 'white',
+    borderRadius: '10px',
+    textAlign: 'center'
+}
+
+const modalContentStyle = {
+    marginBottom:'20px',
+    lineHeight: 1.6,
+    fontFamily: 'Helvetica Neue, Helvetica, Arial, sans-serif',
+    color: 'rgb(102, 102, 102)'
+}
+
+const toolTipContentStyle = {
+    
+}
+
+const FloaterContent = props => {
+    const {instruction, onNextInstruction} = props;
+    return !instruction.isModal ? (
+        <div style={toolTipContentStyle}>
+            {instruction.description?(<span>{instruction.description}</span>):(instruction.customContent&&renderHTML(instruction.customContent))}
+            {instruction.checkUserCode ? (<button className={classnames(styles.nextButton)} onClick={() => onNextInstruction()}>Next</button>) : null}
+        </div>
+    ) :
+        (<div style={modalContainerStyle}>
+            <div dangerouslySetInnerHTML={{ __html: instruction.description||instruction.customContent }} style={modalContentStyle}></div>
+            <button className={classnames(styles.nextButton)} onClick={() => onNextInstruction()}>{instruction.customizedNextButtonText || 'Next'}</button>
+        </div>);
+}
+
+const TutorialFloater = props => {
+    const { currentStep, currentInstruction, onNextInstruction, instruction } = props;
+    return (
+        <Floater
+            content={!instruction.isModal && <FloaterContent instruction={instruction} onNextInstruction={onNextInstruction}/>}
+            component={instruction.isModal &&<FloaterContent instruction={instruction} onNextInstruction={onNextInstruction}/>}
+            disableAnimation
+            event="hover"
+            key={`step_${currentStep}_${currentInstruction}`}
+            offset={5}
+            placement={instruction.floaterPlacement ? instruction.floaterPlacement : "auto"}
+            open={true}
+            styles={{
+                tooltip: {
+                    maxWidth: modalMaxSize[instruction.modalSize || 'default'],
+                    width: "100%"
+                },
+                options: { zIndex: 550 }
+            }}
+            getPopper={popper => {
+                setTimeout(() => popper.instance.scheduleUpdate(), 0);
+            }}
+        >
+            <span style={beaconStyle} className="BeaconTarget">◉</span>
+        </Floater>
+    );
+}
 
 const Tutorial = props => {
-    const { target, currentStep, currentInstruction, steps, onNextInstruction } = props;
+    const { target, currentStep, currentInstruction, steps} = props;
     const instructions = steps[currentStep].instructions;
     const instruction = instructions[currentInstruction];
     return (
         <div className='Tutorial'>
             <Steps current={currentStep} direction={'horizontal'} >
                 {steps.map((step, key) => (
-                    <Steps.Step key={key} title={step.title} description={step.description} />
+                    <Steps.Step key={key} title={step.title} 
+                    // description={step.description} 
+                    />
                 ))}
             </Steps>
-            {props.isDevMode && <button onClick={()=>{
-                props.onMarkInstructionComplete(0,6);
-                }}>step2</button>}
+
+            {props.isDevMode && <button onClick={() => {
+                props.onMarkInstructionComplete(0, 7);
+            }}>step2</button>}
+
             <Popper referenceElement={target ? target : virtualReferenceElement} placement={instruction.beaconAlign}>
                 {({ ref, style, placement, arrowProps, scheduleUpdate }) => (
 
                     <div ref={ref} style={{ ...style, zIndex: 1000 }} data-placement={placement}>
-                        <Floater
-                            content={!instruction.isModal ? (
-                                <div>
-                                    {instruction.description}
-                                    {instruction.checkUserCode ? (<button className={classnames(styles.nextButton)} onClick={() => onNextInstruction()}>Next</button>) : null}
-                                </div>
-                            ) :
-                                (<div>
-                                    <p>{instruction.description}</p>
-                                    <button className={classnames(styles.nextButton)} onClick={() => onNextInstruction()}>{instruction.customizedNextButtonText || 'Next'}</button>
-                                </div>)
-                            }
-                            disableAnimation
-                            event="hover"
-                            key={`step_${currentStep}_${currentInstruction}`}
-                            offset={5}
-                            placement={instruction.floaterPlacement ? instruction.floaterPlacement : "auto"}
-                            open={true}
-                            styles={{
-                                tooltip: {
-                                    maxWidth: 500,
-                                    width: "100%"
-                                },
-                                options: { zIndex: 550 }
-                            }}
-                            getPopper={popper => {
-                                setTimeout(() => popper.instance.scheduleUpdate(), 0);
-                            }}
-
-                        >
-                            <span style={beaconStyle} className="BeaconTarget">
-                                ◉
-                            </span>
-                        </Floater>
+                        <TutorialFloater {...props} instruction={instruction} />
                         <div ref={arrowProps.ref} style={{ ...arrowProps.style }} />
                     </div>
                 )}
--- src/containers/tutorial.jsx
+++ src/containers/tutorial.jsx
@@ -10,74 +10,106 @@ import { addBlocksToWorkspace, testBlocks , workspaceFromXml} from '../lib/hints
 
 const steps = [
     {
-        title: 'Create a custom block',
-        description: `Create a custom block and name it "Swing`,
+        title: 'Learning the Basics of a Custom Block',
+        description: `How does a Custom Block work?`,
         instructions: [
             {
-                description: `Welcome Scratchers! Let's get started!`,
                 selectorExpr: `document.querySelectorAll("div.rc-steps-item-description")[0]`,
                 isModal: true,
+                customContent: `
+                <h3>Welcome Scratchers!</h3> 
+                <div style="text-align: left">
+                <p><b>Procedure</b> is a very powerful concept known in Scratch as a <b>Custom Block</b>.
+                In this tutorial, you will learn to:</p>
+                    <ul>
+                        <li>Master the use of a custom block</li>
+                        <li>Share your custom block with others</li>
+                    </ul>
+                </div>`,
+                modalSize: 'large',
                 beaconAlign: 'right-start',
                 floaterPlacement: 'center',
                 workspaceSetupCode: "<xml xmlns='http://www.w3.org/1999/xhtml'><block type='event_whenflagclicked' id='3GgSEq2C3!UUHbO~!2wG' x='220' y='272'><next><block type='motion_movesteps' id=':L6MsjJxAuqwuVSw^8oO'><value name='STEPS'><shadow type='math_number' id='TXZwH5e3}+[}[RJz#DH)'><field name='NUM'>10</field></shadow></value><next><block type='motion_turnright' id='BKf`Tu?`rziW2g!-*QpM'><value name='DEGREES'><shadow type='math_number' id='JJ-pr9fYpv$me2!%C=K3'><field name='NUM'>15</field></shadow></value><next><block type='motion_pointindirection' id='#u=Td#wrIT0]I*9N:`s/'><value name='DIRECTION'><shadow type='math_angle' id='!z#aH]hyO|NfSDT]%/;}'><field name='NUM'>90</field></shadow></value><next><block type='motion_movesteps' id='m%)5Z67I8g-ofd@LwgX`'><value name='STEPS'><shadow type='math_number' id='+6ij{vIyaYP{9#X,QV/B'><field name='NUM'>10</field></shadow></value><next><block type='motion_turnright' id='%Cqf}+0Pz[Y^7w~Y{{VT'><value name='DEGREES'><shadow type='math_number' id='atI.x%~WQb$X?s.n8:V*'><field name='NUM'>15</field></shadow></value></block></next></block></next></block></next></block></next></block></next></block></xml>",
-                customizedNextButtonText: 'OK'
+                customizedNextButtonText: 'Begin the tutorial'
             },
             {
-                description: `Click My Blocks`,
+                customContent: `Click <b>"My Blocks"</b>`,
                 selectorExpr: `document.querySelector(".scratchCategoryMenu > div:nth-child(9)")`,
                 delayNextInstruction: 600,
             },
             {
-                description: `Click Make a Block`,
+                customContent: `Click <b>"Make a Block"</b>`,
                 selectorExpr: `document.querySelectorAll(".blocklyFlyoutButton")[2]`,
                 beaconAlign: 'right'
             },
             {
-                description: `Give procedure a name and click OK`,
+                customContent: `<p>Give your custom block a meaningful name (e.g., "MOVEMENT"). When you are done, click <b>"OK"</b><p>`,
                 selectorExpr: `document.querySelector("div.ReactModalPortal").querySelector("g.blocklyBlockCanvas")`,
                 triggerNextTarget: `document.querySelector('div.ReactModalPortal').querySelectorAll('button')[1]`,
                 beaconAlign: 'right'
             },
             {
-                description: `Nicely Done! What you see is the signature block that we can define what it does!
-                Let's make it do the MOVEMENT! Copy the code that perform MOVEMENT and snap it to this define block`,
+                customContent: `
+                <p><b>Nicely Done!</b> This is the signature block that you can define what it does!</p>
+                <p>Let's make it do the MOVEMENT!
+                Copy the code that perform MOVEMENT and snap it to this define block</p>`,
                 selectorExpr: `this.workspace.getAllBlocks().find(b=>b.type==='procedures_definition').svgGroup_`,
                 beaconAlign: 'right',
                 floaterPlacement: 'right',
                 checkUserCode: true
             },
             {
-                description: `Great Job!
-                    All of your custom blocks will be available in "My Blocks" category. 
-                    Let's use the custom block you just created!
-                    Let's replace the blocks in the workspace that do the MOVEMENT with the custom block`,
+                customContent: `<p><b>Great Job!</b> All of your custom blocks will be available in <b>"My Blocks"</b> category.</p>
+                    <p> Let's use the custom block you just created!
+                    Let's replace the blocks in the workspace that do the MOVEMENT with the custom block</p>`,
                 selectorExpr: `this.flyout.getAllBlocks().find(b=>b.type==='procedures_call').svgGroup_`,
                 beaconAlign: 'bottom',
                 floaterPlacement: 'bottom',
                 checkUserCode: true
-            }
-        ]
-    }, {
-        title: 'Use the custom block',
-        description: `Copy the highlighted blocks`,
-        instructions: [
+            },
             {
-                description: `Nicely Done! Click on Green Flag to test if everything is still working as expected.`,
+                customContent: `
+                <b>Nicely Done!</b> 
+                <p>Click <b>Green Flag</b> to test if everything is still working as expected.</p>`,
                 selectorExpr: `document.querySelector('.greenFlag')`,
                 // `this.workspace.topBlocks_.find(b=>b.type==='event_whenflagclicked').svgGroup_.children[0]`,
                 beaconAlign: 'left',
                 floaterPlacement: 'left',
                 // workspaceSetupCode to be remove for production 
-                workspaceSetupCode: "<xml xmlns='http://www.w3.org/1999/xhtml'><block type='procedures_definition' id='PxH;Rp_Ia/gX3wGoj7O[' x='-508' y='-285'><statement name='custom_block'><shadow type='procedures_prototype' id='1{}?-zEWx!J3E27pwg2a'><mutation proccode='block name' argumentids='[]' argumentnames='[]' argumentdefaults='[]' warp='false'></mutation></shadow></statement><next><block type='motion_movesteps' id='m%)5Z67I8g-ofd@LwgX`'><value name='STEPS'><shadow type='math_number' id='+6ij{vIyaYP{9#X,QV/B'><field name='NUM'>10</field></shadow></value><next><block type='motion_turnright' id='%Cqf}+0Pz[Y^7w~Y{{VT'><value name='DEGREES'><shadow type='math_number' id='atI.x%~WQb$X?s.n8:V*'><field name='NUM'>15</field></shadow></value></block></next></block></next></block><block type='event_whenflagclicked' id='3GgSEq2C3!UUHbO~!2wG' x='-92' y='13'><next><block type='motion_movesteps' id=':L6MsjJxAuqwuVSw^8oO'><value name='STEPS'><shadow type='math_number' id='TXZwH5e3}+[}[RJz#DH)'><field name='NUM'>10</field></shadow></value><next><block type='motion_turnright' id='BKf`Tu?`rziW2g!-*QpM'><value name='DEGREES'><shadow type='math_number' id='JJ-pr9fYpv$me2!%C=K3'><field name='NUM'>15</field></shadow></value><next><block type='motion_pointindirection' id='#u=Td#wrIT0]I*9N:`s/'><value name='DIRECTION'><shadow type='math_angle' id='!z#aH]hyO|NfSDT]%/;}'><field name='NUM'>90</field></shadow></value></block></next></block></next></block></next></block></xml>"
+                delayNextInstruction: 2000
             },
             {
-                description: `description`,
-                isModal: true
+                customContent: `Looks like it's working just like before! 
+                <p>What's better is that with a meaningful custom block's name, it helps make the code easier to understand.</p>`,
+                isModal: true,
+                floaterPlacement: 'center'
             }
         ]
     }, {
-        title: 'Add parameter to the custom block',
-        description: `Allow your custom block to perform a variation`
+        title: 'Mastering the Custom Block',
+        description: `Passing parameters to your custom block`,
+        instructions: [
+            {
+                isModal: true,
+                floaterPlacement: 'center',
+                customContent: `<h3>You just learned the basics of a custom block!</h3> 
+                <p>What if we want to make the Scratch Cat move a little bit differently?</p>
+                <p>Luckily, we don't need to create another custom block.</p>
+
+                <p>Next you will learn how to use <b>parameters</b> to make a custom block even more powerful.<p/>
+                `,
+                workspaceSetupCode: "<xml xmlns='http://www.w3.org/1999/xhtml'><block type='procedures_definition' id='PxH;Rp_Ia/gX3wGoj7O[' x='-508' y='-285'><statement name='custom_block'><shadow type='procedures_prototype' id='1{}?-zEWx!J3E27pwg2a'><mutation proccode='block name' argumentids='[]' argumentnames='[]' argumentdefaults='[]' warp='false'></mutation></shadow></statement><next><block type='motion_movesteps' id='m%)5Z67I8g-ofd@LwgX`'><value name='STEPS'><shadow type='math_number' id='+6ij{vIyaYP{9#X,QV/B'><field name='NUM'>10</field></shadow></value><next><block type='motion_turnright' id='%Cqf}+0Pz[Y^7w~Y{{VT'><value name='DEGREES'><shadow type='math_number' id='atI.x%~WQb$X?s.n8:V*'><field name='NUM'>15</field></shadow></value></block></next></block></next></block><block type='event_whenflagclicked' id='3GgSEq2C3!UUHbO~!2wG' x='-92' y='13'><next><block type='motion_movesteps' id=':L6MsjJxAuqwuVSw^8oO'><value name='STEPS'><shadow type='math_number' id='TXZwH5e3}+[}[RJz#DH)'><field name='NUM'>10</field></shadow></value><next><block type='motion_turnright' id='BKf`Tu?`rziW2g!-*QpM'><value name='DEGREES'><shadow type='math_number' id='JJ-pr9fYpv$me2!%C=K3'><field name='NUM'>15</field></shadow></value><next><block type='motion_pointindirection' id='#u=Td#wrIT0]I*9N:`s/'><value name='DIRECTION'><shadow type='math_angle' id='!z#aH]hyO|NfSDT]%/;}'><field name='NUM'>90</field></shadow></value></block></next></block></next></block></next></block></xml>",
+                customizedNextButtonText: 'Continue'
+            },
+            {
+                customContent: `Let's`,
+                selectorExpr: `document.querySelectorAll(".blocklyFlyoutButton")[2]`,
+                beaconAlign: 'right'
+            }
+        ]
+    }, {
+        title: 'Sharing your Custom Block with Others',
+        description: ``
     }
 ]
 
@@ -120,6 +152,7 @@ class Tutorial extends React.Component {
                         workspace.clear();
                         // addBlocksToWorkspace(workspace, instruction.workspaceSetupCode); //call it by onWsUpdate
                         workspaceFromXml(workspace, instruction.workspaceSetupCode);
+                        workspace.cleanUp();
                         workspace.scrollCenter();
                     }, 100
                 )
@@ -152,7 +185,6 @@ class Tutorial extends React.Component {
 
     render() {
         const { steps, currentStep, currentInstruction } = this.props.tutorial;
-        console.log(currentStep, currentInstruction);
         if (steps.length > 0) {
             const instruction = steps[currentStep].instructions[currentInstruction];
             if(this.workspace&&instruction.workspaceSetupCode){
